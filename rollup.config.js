20a31e416e561ee119cc7ae34f786b02b6ed301e
Xmader <xmader@outlook.com>
2018-10-10 03:21:28 +0800

    v0.4.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
-// import babel from 'rollup-plugin-babel';
-import metainfo from "./src/metainfo.js";
+import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
+import metainfo from "./src/metainfo.js"
 
 export default {
     input: "main.js",
     output: {
         file: "dist/bring_userscript_to_via.js",
         format: "iife",
-        sourcemap: true,
+        // sourcemap: true,
         banner: metainfo
-    }
+    },
+    plugins: [
+        resolve(),
+        babel()
+    ]
 }
